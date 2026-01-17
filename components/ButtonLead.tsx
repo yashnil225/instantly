eb89f48b41f9a1d8dfe1f96f80fc92b33178f076
yashnil225 <yashnilshukla2@gmail.com>
2026-01-17 19:41:26 +0530

    typo error fix

--- components/ButtonLead.tsx
+++ components/ButtonLead.tsx
@@ -10,7 +10,7 @@ import apiClient from "@/libs/api";
 // For instance: A popup to send a freebie, joining a waitlist, etc.
 // It calls the /api/lead/route.js route and store a Lead document in the database
 const ButtonLead = ({ extraStyle }: { extraStyle?: string }) => {
-  const inputRef = useRef(null);
+  const inputRef = useRef<HTMLInputElement>(null);
   const [email, setEmail] = useState<string>("");
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [isDisabled, setIsDisabled] = useState<boolean>(false);
@@ -25,7 +25,7 @@ const ButtonLead = ({ extraStyle }: { extraStyle?: string }) => {
       toast.success("Thanks for joining the waitlist!");
 
       // just remove the focus on the input
-      inputRef.current.blur();
+      inputRef.current?.blur();
       setEmail("");
       setIsDisabled(true);
     } catch (error) {

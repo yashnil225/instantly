ae11b50582afec46d28765b848f9d70c01ba1039
yashnil225 <yashnilshukla2@gmail.com>
2026-02-10 20:01:14 +0530

    feat: add CreateWorkspaceModal component to allow users to create new workspaces with a name and default opportunity value.

--- components/app/CreateWorkspaceModal.tsx
+++ components/app/CreateWorkspaceModal.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react"
 import { Button } from "@/components/ui/button"
-import { Input } from "@components/ui/input"
+import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import {
     Dialog,
